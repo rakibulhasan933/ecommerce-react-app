2f2784a7787a2ad04ac76f6a2f976271ce99c56e
RAKIBUL <rakibulmd933@gmail.com>
2023-01-05 23:28:08 +0600

    [ADD] link added

--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
@@ -9,29 +10,27 @@ const Navbar = () => {
 						<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 					</label>
 					<ul tabIndex={0} className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-						<li><a href='/'>Women</a></li>
-						<li><a href='/'>Men</a></li>
-						<li><a href='/'>Children</a></li>
-						<li><a href='/'>Stores</a></li>
+						<li><Link to='/'>Women</Link></li>
+						<li><Link to='/'>Men</Link></li>
+						<li><Link to='/'>Children</Link></li>
+						<li><Link to='/'>Stores</Link></li>
 						<li><label tabIndex={0} className="btn btn-ghost btn-circle">
 							<div className="indicator">
 								<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
 								<span className="badge badge-sm indicator-item">0</span>
 							</div>
 						</label></li>
-						<li>
-							<a href="/" className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">Logout</a>
-						</li>
+						<li><Link to="/">Logout</Link></li>
 					</ul>
 				</div>
-				<a href='/' className="text-xl normal-case btn btn-ghost">Walker Store</a>
+				<Link to='/' className="text-xl normal-case btn btn-ghost">Walker Store</Link>
 			</div>
 			<div className="hidden navbar-center lg:flex">
 				<ul className="px-1 menu menu-horizontal">
-					<li><a href='/'>Women</a></li>
-					<li><a href='/'>Men</a></li>
-					<li><a href='/'>Children</a></li>
-					<li><a href='/'>Stores</a></li>
+					<li><Link to='/'>Women</Link></li>
+					<li><Link to='/'>Men</Link></li>
+					<li><Link to='/'>Children</Link></li>
+					<li><Link to='/stores'>Stores</Link></li>
 				</ul>
 			</div>
 			<div className="navbar-end">
@@ -53,14 +52,7 @@ const Navbar = () => {
 						</div>
 					</label>
 					<ul tabIndex={0} className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-						<li>
-							<a className="justify-between">
-								Profile
-								<span className="badge">New</span>
-							</a>
-						</li>
-						<li><a>Settings</a></li>
-						<li><a>Logout</a></li>
+						<li><Link to='/'>Logout</Link></li>
 					</ul>
 				</div>
 			</div>
