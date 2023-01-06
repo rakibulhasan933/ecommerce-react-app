85e1b399b3dcbe222c0b8c635ce534d34ba43f91
RAKIBUL <rakibulmd933@gmail.com>
2023-01-06 22:23:05 +0600

    [ADD] feautred product bio

--- src/pages/FeatureProducts/FeatureProducts.js
+++ src/pages/FeatureProducts/FeatureProducts.js
@@ -39,9 +39,9 @@ const FeatureProducts = () => {
 		<div className='p-6 m-12 bg-base-100'>
 			<div className="flex flex-row py-11">
 				<div className="px-12 text-2xl font-semibold text-orange-400 basis-1/2">Featured Products</div>
-				<div className="basis-1/2"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus nam harum quaerat libero enim ipsam dolore! Officia perferendis voluptatum maxime, nobis quas unde deleniti quisquam quaerat aperiam labore optio molestiae! </div>
+				<div className="basis-1/2">Today's feather outfits still boast nods to golden-era glamour and vintage boudoir style, but the options are fresh, colorful, seemingly endless, and can even lean casual. Options include shoes dotted with plumes, pants hemmed with quills, feather going-out tops, and of course feathery dresses, too</div>
 			</div>
-			<div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+			<div className="grid grid-cols-1 gap-4 px-10 md:grid-cols-4">
 				{
 					data.map((item) => <FeatureCard item={item} />)
 				}
