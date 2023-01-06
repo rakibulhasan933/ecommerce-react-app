2faf24090486efdb4eb72b7c948c20973f82aa6e
RAKIBUL <rakibulmd933@gmail.com>
2023-01-07 00:46:22 +0600

    [ADD] flexbox photo responsive

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 const Categories = () => {
 	return (
-		<div style={{ height: `calc(100vh - 100px)` }} className='m-6 bg-slate-100'>
-			<div className="grid grid-cols-1 gap-0 md:grid-cols-4">
-				<div className="">
-					<img src="https://i.ibb.co/82ZytFc/pexels-pixabay-458766.jpg" alt="men" />
+		<div style={{ height: `calc(100vh - 30px)` }} className='m-6 bg-slate-100'>
+			<div className="grid grid-flow-row-dense grid-cols-4">
+				<div>
+					<img className='object-cover w-full h-full' src="https://i.ibb.co/82ZytFc/pexels-pixabay-458766.jpg" alt="men" />
 				</div>
 				<div className="row-span-3">
-					<img src="https://i.ibb.co/64Bj5HL/pexels-light-vibe-990675.jpg" alt="men" />
+					<img className='object-cover w-full h-full' src="https://i.ibb.co/64Bj5HL/pexels-light-vibe-990675.jpg" alt="men" />
 				</div>
 				<div className="">
-					<img src="https://i.ibb.co/fQ6d0np/pexels-ksenia-chernaya-3965543.jpg" alt="men" />
+					<img className='object-cover w-full h-full' src="https://i.ibb.co/fQ6d0np/pexels-ksenia-chernaya-3965543.jpg" alt="men" />
 				</div>
 				<div className="">
-					<img src="https://i.ibb.co/RPcsH4W/pexels-helena-lopes-1049317.jpg" alt="men" />
+					<img className='object-cover w-full h-full' src="https://i.ibb.co/RPcsH4W/pexels-helena-lopes-1049317.jpg" alt="men" />
 				</div>
 				<div className="">
-					<img src="https://i.ibb.co/30GHm3N/pexels-r-fera-432059.jpg" alt="men" />
+					<img className='object-cover w-full h-full' src="https://i.ibb.co/hL09fH9/pexels-gianni-orefice-4065967.jpg" alt="men" />
 				</div>
 				<div className="col-span-2">
-					<img src="https://i.ibb.co/YX8WB3Q/pexels-burst-373964.jpg" alt="men" />
+					<img className='object-cover w-full h-full' src="https://i.ibb.co/YX8WB3Q/pexels-burst-373964.jpg" alt="men" />
 				</div>
 			</div>
 		</div>
