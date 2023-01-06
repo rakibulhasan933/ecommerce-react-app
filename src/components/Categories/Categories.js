d6e5803d0abfbcf3eae1f831fe6f73a357a3f36a
RAKIBUL <rakibulmd933@gmail.com>
2023-01-07 01:14:54 +0600

    [ADD] flexbox responsive

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Categories = () => {
 	return (
-		<div style={{ height: `calc(100vh - 30px)` }} className='m-6 bg-slate-100'>
+		<div style={{ height: `calc(100vh - 50px)` }} className='m-6 bg-slate-100'>
 			<div className="grid grid-flow-row-dense grid-cols-4">
-				<div>
+				<div className='relative '>
 					<img className='object-cover w-full h-full' src="https://i.ibb.co/82ZytFc/pexels-pixabay-458766.jpg" alt="men" />
+					<div className="absolute p-1 top-1/2 bottom-1/2 left-1/2">
+						<h2 className=' bg-slate-100'>WOMAN</h2>
+					</div>
 				</div>
 				<div className="row-span-3">
 					<img className='object-cover w-full h-full' src="https://i.ibb.co/64Bj5HL/pexels-light-vibe-990675.jpg" alt="men" />
