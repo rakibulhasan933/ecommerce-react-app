678f2722bfbbe5c9aacd32c46b06596657367dbc
RAKIBUL <rakibulmd933@gmail.com>
2023-01-07 01:28:21 +0600

    [ADD] flexbox final

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -10,20 +10,35 @@ const Categories = () => {
 						<h2 className=' bg-slate-100'>WOMAN</h2>
 					</div>
 				</div>
-				<div className="row-span-3">
+				<div className="relative row-span-3">
 					<img className='object-cover w-full h-full' src="https://i.ibb.co/64Bj5HL/pexels-light-vibe-990675.jpg" alt="men" />
+					<div className="absolute p-1 top-1/2 bottom-1/2 left-1/2">
+						<h2 className=' bg-slate-100'>NEW SEASON</h2>
+					</div>
 				</div>
-				<div className="">
+				<div className="relative ">
 					<img className='object-cover w-full h-full' src="https://i.ibb.co/fQ6d0np/pexels-ksenia-chernaya-3965543.jpg" alt="men" />
+					<div className="absolute p-1 top-1/2 bottom-1/2 left-1/2">
+						<h2 className=' bg-slate-100'>SALE</h2>
+					</div>
 				</div>
-				<div className="">
+				<div className="relative ">
 					<img className='object-cover w-full h-full' src="https://i.ibb.co/RPcsH4W/pexels-helena-lopes-1049317.jpg" alt="men" />
+					<div className="absolute p-1 top-1/2 bottom-1/2 left-1/2">
+						<h2 className=' bg-slate-100'>MEN</h2>
+					</div>
 				</div>
-				<div className="">
+				<div className="relative ">
 					<img className='object-cover w-full h-full' src="https://i.ibb.co/hL09fH9/pexels-gianni-orefice-4065967.jpg" alt="men" />
+					<div className="absolute p-1 top-1/2 bottom-1/2 left-1/2">
+						<h2 className=' bg-slate-100'>ACCSORIES</h2>
+					</div>
 				</div>
-				<div className="col-span-2">
+				<div className="relative col-span-2">
 					<img className='object-cover w-full h-full' src="https://i.ibb.co/YX8WB3Q/pexels-burst-373964.jpg" alt="men" />
+					<div className="absolute p-1 top-1/2 bottom-1/2 left-1/2">
+						<h2 className=' bg-slate-100'>SHOES</h2>
+					</div>
 				</div>
 			</div>
 		</div>
