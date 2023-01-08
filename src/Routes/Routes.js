1da499ff6133231e64254f2db3fe190cbc86834c
RAKIBUL <rakibulmd933@gmail.com>
2023-01-08 14:36:42 +0600

    [ADD] product demo

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layout/Main';
-import About from '../pages/About/About';
+// import About from '../pages/About/About';
 import Home from '../pages/Home/Home';
+import Product from '../pages/Product/Product';
 import DisplayError from '../pages/Shared/DisplayError';
 
 const router = createBrowserRouter([
@@ -12,12 +13,12 @@ const router = createBrowserRouter([
 		errorElement: <DisplayError />,
 		children: [
 			{
-				path: '/',
+				path: '/home',
 				element: <Home />
 			},
 			{
-				path: '/about',
-				element: <About />
+				path: '/',
+				element: <Product />
 			}
 		]
 	}
---
+++ src/pages/Product/Product.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Product = () => {
+	return (
+		<div className='flex m-6'>
+			<div className="basis-1/12">
+				left
+			</div>
+			<div className=" basis-11/12">
+				right
+			</div>
+		</div>
+	);
+};
+
+export default Product;
