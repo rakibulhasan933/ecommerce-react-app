d93a1b184b1c078934fc9e74e8961702c51d9244
RAKIBUL <rakibulmd933@gmail.com>
2023-01-08 14:43:30 +0600

    [ADD] photo

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Product = () => {
+
+	const [selectedImg, setSelectedImg] = useState(0);
+
+	const images = [
+		"https://i.ibb.co/nbYfxDG/pexels-dejan-krstevski-1578997.jpg",
+		"https://i.ibb.co/mCx6M6z/pexels-andrea-piacquadio-3772506.jpg"
+	];
+
 	return (
 		<div className='flex m-6'>
 			<div className="basis-1/12">
-				left
+				<img src={images[0]} alt="men" />
+				<img src={images[1]} alt="men" />
 			</div>
 			<div className=" basis-11/12">
 				right
