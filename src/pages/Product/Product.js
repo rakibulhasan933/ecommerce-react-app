f6fa4a703c967aa3da091e0716add6d96dbeae5d
RAKIBUL <rakibulmd933@gmail.com>
2023-01-08 15:31:07 +0600

    [ADD] photos completed

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -5,15 +5,15 @@ const Product = () => {
 	const [selectedImg, setSelectedImg] = useState(0);
 
 	const images = [
-		"https://i.ibb.co/nbYfxDG/pexels-dejan-krstevski-1578997.jpg",
+		"https://i.ibb.co/GWTyTTf/skirt1.jpg",
 		"https://i.ibb.co/mCx6M6z/pexels-andrea-piacquadio-3772506.jpg"
 	];
 
 	return (
 		<div style={{ height: `calc(100vh - 40px)` }} className='flex m-6'>
 			<div className="flex flex-col gap-4 px-1 basis-1/12">
-				<img className='cursor-pointer' src={images[0]} alt="men" onClick={e => setSelectedImg(0)} />
-				<img className='cursor-pointer' src={images[1]} alt="men" onClick={e => setSelectedImg(1)} />
+				<img className='w-full h-32 cursor-pointer' src={images[0]} alt="men" onClick={e => setSelectedImg(0)} />
+				<img className='w-full h-32 cursor-pointer' src={images[1]} alt="men" onClick={e => setSelectedImg(1)} />
 			</div>
 			<div className="flex flex-row mx-4 basis-11/12">
 				<div className="mx-2 basis-1/2">
