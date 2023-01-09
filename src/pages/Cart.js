519ebbb0ce0247e97e0b0aa0fecbc694ada0657f
RAKIBUL <rakibulmd933@gmail.com>
2023-01-10 00:05:42 +0600

    [ADD]cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,61 @@
+import { DeleteOutlined } from '@mui/icons-material';
 import React from 'react';
 
 const Cart = () => {
+	const data = [
+		{
+			id: 1,
+			img: "https://i.ibb.co/hL09fH9/pexels-gianni-orefice-4065967.jpg",
+			title: "Long Sleeve Graphic T-Shirt",
+			oldPrice: 30,
+			price: 20,
+		},
+		{
+			id: 2,
+			img: "https://i.ibb.co/NmYXzMJ/coat1.jpg",
+			title: "Jacket",
+			oldPrice: 40,
+			price: 30,
+		},
+		{
+			id: 3,
+			img: "https://i.ibb.co/mCx6M6z/pexels-andrea-piacquadio-3772506.jpg",
+			title: "Coat",
+			oldPrice: 35,
+			price: 25,
+		},
+		{
+			id: 4,
+			img: "https://i.ibb.co/nbYfxDG/pexels-dejan-krstevski-1578997.jpg",
+			title: "Jeans Jacket",
+			oldPrice: 40,
+			price: 25,
+		}
+
+	];
 	return (
 		<div className='absolute z-40 p-5 bg-white shadow-md right-5 top-20'>
-			<h2>Shopping Cart</h2>
+			<h2>Products in your Cart</h2>
+			<div className="flex flex-col m-2">
+				{
+					data.map((item) => (
+						<div className='flex flex-row items-center mb-2'>
+							<img className='object-cover w-20 h-24 mr-2' src={item?.img} alt="products" />
+							<div className="flex flex-col mr-4 ">
+								<h1>{item?.title}</h1>
+								<h5>{item?.price} </h5>
+							</div>
+							<DeleteOutlined className='text-red-500 cursor-pointer' />
+						</div>
+					))
+				}
+			</div>
+			<div className="flex flex-row justify-between m-3">
+				<span className="">TOTAL</span>
+				<span className="">$ 1234</span>
+			</div>
+			<button className=''>PROCEED TO CHECKOUT</button>
+			<h3 className=''>Reset Cart</h3>
 		</div>
 	);
 };
