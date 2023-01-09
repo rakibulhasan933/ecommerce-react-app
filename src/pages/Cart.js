f0cf254936dba11a198bddf47d7d419943e6b702
RAKIBUL <rakibulmd933@gmail.com>
2023-01-09 22:43:51 +0600

    [ADD] cart open

---
+++ src/pages/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+	return (
+		<div className='absolute z-40 p-5 bg-white shadow-md right-5 top-20'>
+			<h2>Shopping Cart</h2>
+		</div>
+	);
+};
+
+export default Cart;
--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+import Cart from '../Cart';
+
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false);
+
 	return (
 		<div className="navbar bg-base-100">
 			<div className="navbar-start">
@@ -40,9 +45,8 @@ const Navbar = () => {
 					</div>
 				</div>
 				<label tabIndex={0} className="ml-1 btn btn-ghost btn-circle">
-					<div className="indicator">
-						<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-						<span className="badge badge-sm indicator-item">0</span>
+					<div onClick={() => setOpen(!open)} className="indicator">
+						<AddShoppingCartOutlinedIcon />
 					</div>
 				</label>
 				<div className="dropdown dropdown-end">
@@ -55,6 +59,7 @@ const Navbar = () => {
 						<li><Link to='/'>Logout</Link></li>
 					</ul>
 				</div>
+				{open && <Cart />}
 			</div>
 		</div>
 	);
