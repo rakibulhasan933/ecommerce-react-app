b22d9aba36977d6deb893c76f3daa27e8de9e61c
RAKIBUL <rakibulmd933@gmail.com>
2023-01-15 17:07:01 +0600

    [ADD] sppiner added

---
+++ src/pages/Shared/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loading = () => {
+	return (
+		<div className='flex items-center justify-center h-screen '>
+			<button type="button" class="bg-indigo-500 ..." disabled>
+				<svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+				</svg>
+				Processing...
+			</button>
+		</div>
+	);
+};
+
+export default Loading;
