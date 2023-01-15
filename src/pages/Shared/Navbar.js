91101c3ecd01e07f5a2d8f3b07412bb08f7e6f26
RAKIBUL <rakibulmd933@gmail.com>
2023-01-15 19:23:40 +0600

    [ADD] navbar completed

--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
 import Cart from '../Cart';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 
 const Navbar = () => {
+	const [user] = useAuthState(auth);
+
+	const logout = () => {
+		signOut(auth);
+	}
+
 	const [open, setOpen] = useState(false);
 
 	return (
@@ -19,13 +28,6 @@ const Navbar = () => {
 						<li><Link to='/'>Men</Link></li>
 						<li><Link to='/'>Children</Link></li>
 						<li><Link to='/'>Stores</Link></li>
-						<li><label tabIndex={0} className="btn btn-ghost btn-circle">
-							<div className="indicator">
-								<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-								<span className="badge badge-sm indicator-item">0</span>
-							</div>
-						</label></li>
-						<li><Link to="/">Logout</Link></li>
 					</ul>
 				</div>
 				<Link to='/' className="text-xl normal-case btn btn-ghost">Walker Store</Link>
@@ -49,16 +51,22 @@ const Navbar = () => {
 						<AddShoppingCartOutlinedIcon />
 					</div>
 				</label>
-				<div className="dropdown dropdown-end">
+				{user?.uid ? <div className="dropdown dropdown-end">
 					<label tabIndex={0} className="btn btn-ghost btn-circle avatar">
 						<div className="w-10 ml-1 rounded-full">
-							<img src="https://placeimg.com/80/80/people" alt='men' />
+							<img src={user?.photoURL} alt='men' />
 						</div>
 					</label>
 					<ul tabIndex={0} className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-						<li><Link to='/'>Logout</Link></li>
+						<li><button onClick={logout}>Logout</button></li>
 					</ul>
-				</div>
+				</div> :
+					<div className="hidden navbar-center lg:flex">
+						<ul className="px-1 menu menu-horizontal">
+							<li><Link to='login'>Login</Link> </li>
+						</ul>
+					</div>
+				}
 				{open && <Cart />}
 			</div>
 		</div>
