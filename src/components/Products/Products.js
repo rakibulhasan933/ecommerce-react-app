0045abffe64ccd18801cdb11cb76f99478fef908
RAKIBUL <rakibulmd933@gmail.com>
2023-01-16 23:13:32 +0600

    [ADD] home page updated

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,39 +2,6 @@ import React, { useState } from 'react';
 import FeatureCard from '../FeatureCard/FeatureCard';
 
 const Products = () => {
-	const data = [
-		{
-			id: 1,
-			img: "https://i.ibb.co/hL09fH9/pexels-gianni-orefice-4065967.jpg",
-			title: "Long Sleeve Graphic T-Shirt",
-			isNew: true,
-			oldPrice: 30,
-			price: 20,
-		},
-		{
-			id: 2,
-			img: "https://i.ibb.co/NmYXzMJ/coat1.jpg",
-			title: "Jacket",
-			isNew: true,
-			oldPrice: 40,
-			price: 30,
-		},
-		{
-			id: 3,
-			img: "https://i.ibb.co/mCx6M6z/pexels-andrea-piacquadio-3772506.jpg",
-			title: "Coat",
-			oldPrice: 35,
-			price: 25,
-		},
-		{
-			id: 4,
-			img: "https://i.ibb.co/nbYfxDG/pexels-dejan-krstevski-1578997.jpg",
-			title: "Jeans Jacket",
-			oldPrice: 40,
-			price: 25,
-		}
-
-	];
 
 	const [priceValue, setPriceValue] = useState(0);
 
@@ -75,16 +42,7 @@ const Products = () => {
 					</div>
 				</div>
 				<div className='p-4 basis-3/4'>
-					<div className="grid grid-flow-row-dense grid-cols-3 gap-2 py-8 mx-8">
-						<div className="h-48 col-span-3">
-							<img className='w-full h-48' src="https://i.ibb.co/jgD95P5/pexels-ksenia-chernaya-3965539.jpg" alt="men" />
-						</div>
-					</div>
-					<div className="grid h-40 grid-cols-1 gap-4 px-10 md:grid-cols-4">
-						{
-							data.map((item) => <FeatureCard key={item.id} item={item} />)
-						}
-					</div>
+
 				</div>
 			</div>
 		</div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Categories from '../../components/Categories/Categories';
 import Contacts from '../../components/Contacts/Contacts';
-import Products from '../../components/Products/Products';
 import Slider from '../../components/Slider/Slider';
 import FeatureProducts from '../FeatureProducts/FeatureProducts';
 
@@ -11,7 +10,6 @@ const Home = () => {
 			<Slider />
 			<FeatureProducts />
 			<Categories />
-			<Products />
 			<Contacts />
 		</div>
 	);
