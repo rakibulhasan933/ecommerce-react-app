8c914a296b8add93e777cadfe9e14bc41eb1ae6c
RAKIBUL <rakibulmd933@gmail.com>
2023-01-16 17:09:23 +0600

    [ADD] cart improved

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -15,19 +15,19 @@ const Product = () => {
 	];
 
 	return (
-		<div style={{ height: `calc(100vh - 40px)` }} className='flex m-6'>
+		<div className='flex m-6'>
 			<div className="flex flex-col gap-4 px-1 basis-1/12">
 				<img className='w-full h-32 cursor-pointer' src={images[0]} alt="men" onClick={e => setSelectedImg(0)} />
 				<img className='w-full h-32 cursor-pointer' src={images[1]} alt="men" onClick={e => setSelectedImg(1)} />
 			</div>
 			<div className="flex flex-row mx-4 basis-11/12">
 				<div className="mx-2 basis-1/2">
-					<img className='h-full' src={images[selectedImg]} alt="men" />
+					<img className='h-3/4' src={images[selectedImg]} alt="men" />
 				</div>
 				<div className="px-2 basis-1/2">
-					<h1 className='m-2 text-2xl font-medium'>Long Sleeve Graphic T-Shirt</h1>
+					<h1 className='m-2 text-2xl font-medium tracking-tight'>Long Sleeve Graphic T-Shirt</h1>
 					<h3 className='m-2 text-xl text-blue-400' >$ 20</h3>
-					<p className='m-2 text-base font-normal leading-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia beatae consequatur expedita, magnam reprehenderit ab quibusdam laboriosam, esse veniam natus et nulla deserunt accusamus hic asperiores omnis facilis, officia placeat?</p>
+					<h3 className='m-1 text-base font-medium tracking-tight'>Color : Black</h3>
 					<div className="m-4">
 						<button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : (prev - 1)))} className='p-1 mr-2 border-2 bg-slate-200'><RemoveOutlinedIcon /></button>
 						<span className="mr-2">{quantity}</span>
@@ -35,11 +35,11 @@ const Product = () => {
 					</div>
 					<button className='p-2 mx-2 my-4 font-medium text-white bg-blue-500 border cursor-pointer'><span><AddShoppingCartOutlinedIcon /></span> ADD TO CART</button>
 					<div className="flex flex-row p-2 m-3 text-blue-500">
-						<button className='mr-2 text-base font-normal cursor-pointer' ><FavoriteBorderOutlined />ADD TO WISH LIST</button>
-						<button className='text-base font-normal cursor-pointer'> <BalanceOutlined />ADD TO COMPARE</button>
+						<button className='mr-2 text-base font-normal tracking-tight cursor-pointer' ><FavoriteBorderOutlined />ADD TO WISH LIST</button>
+						<button className='text-base font-normal tracking-tight cursor-pointer'> <BalanceOutlined />ADD TO COMPARE</button>
 					</div>
 					<h2 className='m-2 text-lg font-semibold tracking-tight'>Product Details</h2>
-					<div className="flex flex-row mx-3">
+					<div className="flex flex-row m-3">
 						<div className="mx-2">
 							<h4 className='text-base font-medium tracking-tight'>Fabric Type</h4>
 							<h4 className='text-base font-medium tracking-tight'>Care Instructions</h4>
@@ -53,10 +53,10 @@ const Product = () => {
 							<h4 className='text-base font-thin tracking-tight'>Bangladesh</h4>
 						</div>
 					</div>
-					<hr className='w-full bg-gray-400' />
-					<h2 className='text-base font-medium tracking-tight'>About this Item</h2>
+					<hr className='w-full bg-gray-600' />
+					<h2 className='m-2 text-lg font-medium tracking-tight'>About this Item</h2>
 					<div className="m-2">
-
+						<p className='font-normal tracking-tighter lowercase'>BREAKING FASHION BOUNDARIES SINCE 2007: [BLANKYNYC] exploded onto the fashion scene with a ramped up urban attitude and stripped-down irreverent edge, creating approachable clothing and obsession-worthy fits, fabrics, and designs without sacrificing quality</p>
 					</div>
 				</div>
 			</div>
