ea3f0c70c3ed9692efee8dcfeae881e1609b4742
RAKIBUL <rakibulmd933@gmail.com>
2023-01-17 00:09:34 +0600

    [ADD] data load products

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,17 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../../components/Sidebar';
+import Loading from '../Shared/Loading';
 
 const Products = () => {
+	const [products, setProducts] = useState([]);
+	const [isLoading, setLoading] = useState(false);
+	console.log(products);
 
+	useEffect(() => {
+		setLoading(true);
+		fetch('http://localhost:5000/products')
+			.then(res => res.json())
+			.then(data => {
+				setProducts(data);
+				setLoading(false);
+			});
+	}, []);
 	return (
-		<div style={{ height: `calc(100vh - 40px)` }} className="m-6 bg-slate-100">
+		<div className="h-screen m-6 bg-slate-100">
 			<div className="flex flex-col lg:flex-row">
 				<div className='p-8 my-8 basis-1/12'>
 					<Sidebar />
 				</div>
-				<div className='p-4 basis-11/12'>
+				{isLoading ? <Loading /> : <div className='p-4 basis-11/12'>
 
-				</div>
+				</div>}
 			</div>
 		</div>
 	);
