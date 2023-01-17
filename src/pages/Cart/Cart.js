ec96424e4afe02fdfc80e740c240027ec4d71930
RAKIBUL <rakibulmd933@gmail.com>
2023-01-18 00:57:41 +0600

    [ADD] total price output

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -4,7 +4,14 @@ import { useSelector } from 'react-redux';
 
 const Cart = () => {
 	const products = useSelector((state) => state.cart.products);
-	console.log(products);
+
+	const totalPrice = () => {
+		let total = 0;
+		products.forEach((item) => {
+			total += item.quantity * item.price;
+		});
+		return total.toFixed(2);
+	};
 
 	return (
 		<div className='absolute z-40 p-5 bg-white shadow-md right-5 top-20'>
@@ -13,10 +20,10 @@ const Cart = () => {
 				{
 					products?.map((item) => (
 						<div key={item.id} className='flex flex-row items-center mb-2 shadow'>
-							<img className='object-cover w-20 h-24 mr-2' src={item?.img} alt="products" />
+							<img className='object-cover w-20 h-24 mr-2' src={`data:image/png;base64,${item?.image}`} alt="products" />
 							<div className="flex flex-col mr-4 ">
-								<h1 className='text-base font-normal'>{item?.title}</h1>
-								<h5 className='text-base font-medium text-blue-500'>1 X ${item?.price} </h5>
+								<h1 className='text-base font-normal'>{item?.title.slice(0, 15)}</h1>
+								<h5 className='text-base font-medium text-blue-500'>{item.quantity}X${item?.price} </h5>
 							</div>
 							<DeleteOutlined className='mx-4 text-6xl text-red-500 cursor-pointer' />
 						</div>
@@ -25,7 +32,7 @@ const Cart = () => {
 			</div>
 			<div className="flex flex-row justify-between m-3">
 				<span className="text-lg font-medium">TOTAL</span>
-				<span className="text-lg font-medium text-blue-500">$ 1234</span>
+				<span className="text-lg font-medium text-blue-500">${totalPrice()}</span>
 			</div>
 			<button className='flex items-center justify-center w-64 gap-5 p-2 mb-2 font-medium text-white bg-blue-500 border-none'>PROCEED TO CHECKOUT</button>
 			<h3 className='m-2 text-lg font-normal text-red-600 cursor-pointer'>Reset Cart</h3>
