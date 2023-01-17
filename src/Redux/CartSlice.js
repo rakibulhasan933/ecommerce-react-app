35d0ef745d7064310ae6004133c78b8a8520d5af
RAKIBUL <rakibulmd933@gmail.com>
2023-01-17 23:38:47 +0600

    [ADD] add to cart

---
+++ src/Redux/CartSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+	products: [],
+};
+
+export const cartSlice = createSlice({
+	name: 'cart',
+	initialState,
+	reducers: {
+		addToCart: (state, action) => {
+			const item = state.products.find((item) => item.id === action.payload.id);
+			if (item) {
+				item.quantity += action.payload.quantity;
+			} else {
+				state.products.push(action.payload);
+			}
+		},
+		decrement: (state) => {
+			state.value -= 1
+		},
+		incrementByAmount: (state, action) => {
+			state.value += action.payload
+		},
+	},
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = cartSlice.actions
+
+export default cartSlice.reducer;
