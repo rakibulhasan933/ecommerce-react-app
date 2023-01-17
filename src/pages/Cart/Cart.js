8a593ada657acb2c8b1abfb613737e1e5c39310e
RAKIBUL <rakibulmd933@gmail.com>
2023-01-18 00:19:11 +0600

    [ADD] cart show

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -3,45 +3,16 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Cart = () => {
-	const products = useSelector((state) => console.log(state));
-	const data = [
-		{
-			id: 1,
-			img: "https://i.ibb.co/hL09fH9/pexels-gianni-orefice-4065967.jpg",
-			title: "Long Sleeve Graphic T-Shirt",
-			oldPrice: 30,
-			price: 20,
-		},
-		{
-			id: 2,
-			img: "https://i.ibb.co/NmYXzMJ/coat1.jpg",
-			title: "Jacket",
-			oldPrice: 40,
-			price: 30,
-		},
-		{
-			id: 3,
-			img: "https://i.ibb.co/mCx6M6z/pexels-andrea-piacquadio-3772506.jpg",
-			title: "Coat",
-			oldPrice: 35,
-			price: 25,
-		},
-		{
-			id: 4,
-			img: "https://i.ibb.co/nbYfxDG/pexels-dejan-krstevski-1578997.jpg",
-			title: "Jeans Jacket",
-			oldPrice: 40,
-			price: 25,
-		}
+	const products = useSelector((state) => state.cart.products);
+	console.log(products);
 
-	];
 	return (
 		<div className='absolute z-40 p-5 bg-white shadow-md right-5 top-20'>
 			<h2 className='m-2 text-xl font-semibold text-center text-orange-400'>Products in your Cart</h2>
 			<div className="flex flex-col m-2">
 				{
-					data.map((item) => (
-						<div className='flex flex-row items-center mb-2 shadow'>
+					products?.map((item) => (
+						<div key={item.id} className='flex flex-row items-center mb-2 shadow'>
 							<img className='object-cover w-20 h-24 mr-2' src={item?.img} alt="products" />
 							<div className="flex flex-col mr-4 ">
 								<h1 className='text-base font-normal'>{item?.title}</h1>
